492cde9dfc4eb30045cfbcbb7696c9fa98fa5c78
PabloCarrai <pablohernanjuan@hotmail.com>
2025-08-21 16:03:36 -0300

    103

---
+++ Funciones/Ejercicio-108.py
@@ -0,0 +1,18 @@
+"""  
+Confeccionar una aplicacion que muestre una presentacion en pantalla
+del programa. Solicite la carga de dos valores ymuestre la suma. 
+Mostrar finalmente un mensaje de despedida del programa. 
+Implementar estas activiadades en tres funciones
+"""
+
+#   definimos una funcion
+
+
+def presentacion():
+    print("-------------------------------------------------------")
+    print("Programa que permite cargar dos valores por teclado")
+    print("Efectua la suma de los valores y muestra su resultado")
+    print("-------------------------------------------------------")
+
+
+presentacion()
