8f3231465178c1c5ea9e19a7102dd503d83e9a25
PabloCarrai <pablohernanjuan@hotmail.com>
2025-09-06 12:03:10 -0300

    188

--- POO/Ejercicio-188.py
+++ POO/Ejercicio-188.py
@@ -18,21 +18,22 @@ class Triangulo:
                 f"Valores lado 1: {self.lado} lado 2: {self.lado1} lado 3: {self.lado2}")
             print(f"El mayor es {self.lado}")
         else:
-            if ((self.lado1 > self.lado) and (self.lado1 > self.lado2)):
+            if ((self.lado1 > self.lado2)):
                 print(
                     f"Valores lado 1: {self.lado} lado 2: {self.lado1} lado 3: {self.lado2}")
                 print(f"El mayor es {self.lado1}")
             else:
-                if ((self.lado2 > self.lado) and (self.lado2 > self.lado1)):
-                    print(
-                        f"Valores lado 1: {self.lado} lado 2: {self.lado1} lado 3: {self.lado2}")
-                    print(f"El mayor es {self.lado2}")
+                print(
+                    f"Valores lado 1: {self.lado} lado 2: {self.lado1} lado 3: {self.lado2}")
+                print(f"El mayor es {self.lado2}")
 
     def mostrarEquilatero(self):
         if ((self.lado == self.lado1) and (self.lado1 == self.lado2) and (self.lado == self.lado2)):
             print(
                 f"Valores lado 1: {self.lado} lado 2: {self.lado1} lado 3: {self.lado2}")
-            print("Es un equilatero  ")
+            print("Es Equilatero  ")
+        else:
+            print("No es Equilatero")
 
 
 triangulo1 = Triangulo()
