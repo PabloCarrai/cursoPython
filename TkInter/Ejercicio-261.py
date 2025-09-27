1512ba267bea56978c1ee75bef468cfa090d7662
PabloCarrai <pablohernanjuan@hotmail.com>
2025-09-27 15:27:34 -0300

    Ahora si anda cargar imagen

--- TkInter/Ejercicio-261.py
+++ TkInter/Ejercicio-261.py
@@ -11,10 +11,10 @@ class Aplicacion:
         self.canvas1 = tk.Canvas(
             self.ventana, width=700, height=400, background="black")
         self.canvas1.grid(column=0, row=1, padx=10, pady=10)
-
-        archivo1 = tk.PhotoImage(file="/home/ed/cursoPython/TkInter/png1.png")
-        self.canvas1.create_image(300, 100, archivo1, anchor="nw")
-
+        #   Hay que usar gif, y tambien ruta absoluta
+        archivo1 = tk.PhotoImage(file="/home/ed/cursoPython/TkInter/1.gif")
+        #   Aca muestro la imagen
+        self.canvas1.create_image(300, 100, image=archivo1, anchor="nw")
         self.ventana.mainloop()
 
 
