52dc10c38dab8ddbc10f5d40f412631be6ab120c
PabloCarrai <pablohernanjuan@hotmail.com>
2025-10-02 19:30:10 +0000

    Lo hice como me salio. Onda difiere mucho de lo que resulto hacer este cordobes que es un genio. Pero me mande solo y anduvo. Vere para los otros refactorizar y si me sale separar la logica de la db

--- Bases-Datos/Ejercicio-279.py
+++ Bases-Datos/Ejercicio-279.py
@@ -71,10 +71,16 @@ class Aplicacion:
         )
         self.botonConsultar.grid(column=1, row=3, padx=10, pady=10)
 
+        #   Por defecto tienen que estar desabilitados
+        self.entradaDescripcion1.config(state="disabled")
+        self.entradaPrecio1.config(state="disabled")
+
         #   Tercer LabelFrame
         self.labelframe2 = ttk.LabelFrame(self.ventana, text="Articulo:")
 
-        self.botonListadoCompleto = Button(self.labelframe2, text="Listado Completo")
+        self.botonListadoCompleto = Button(
+            self.labelframe2, text="Listado Completo", command=self.listadoCompleto
+        )
         self.botonListadoCompleto.grid(column=0, row=1, padx=10, pady=10)
 
         self.areaTexto = sc.ScrolledText(self.labelframe2, width=40, height=10)
@@ -107,10 +113,48 @@ class Aplicacion:
         conexion.commit()
         conexion.close()
         ms.showinfo(message="Registro insertado", title="Insercion Correcta")
-        self.entradaDescripcion.delete(0,END)
-        self.entradaPrecio.delete(0,END)
+        self.entradaDescripcion.delete(0, END)
+        self.entradaPrecio.delete(0, END)
 
     def consultarCodigo(self):
+        self.entradaDescripcion1.config(state="normal")
+        self.entradaPrecio1.config(state="normal")
+        conexion = mysql.connector.connect(
+            user="root",
+            passwd="SomosDeCarn3",
+            host="192.168.0.222",
+            port=3307,
+            database="db1",
+        )
+        cursor = conexion.cursor()
+        cursor.execute(
+            "select descripcion, precio from articulos where codigo=%s",
+            [self.datoentradaCodigo.get()],
+        )
+        for fila in cursor:
+            self.entradaDescripcion1.delete(0, END)
+            self.entradaPrecio1.delete(0, END)
+            self.entradaDescripcion1.insert(0, f"{fila[0]}")
+            self.entradaPrecio1.insert(0, f"{fila[1]}")
+        conexion.close()
+
+    def listadoCompleto(self):
+        self.areaTexto.delete("1.0", END)
+        conexion = mysql.connector.connect(
+            user="root",
+            passwd="SomosDeCarn3",
+            host="192.168.0.222",
+            port=3307,
+            database="db1",
+        )
+        cursor = conexion.cursor()
+        cursor.execute("select * from articulos")
+        for fila in cursor:
+            self.areaTexto.insert(
+                END, f"Codigo: {fila[0]}\n Descripcion: {fila[1]}\n Precio: {fila[2]}\n"
+            )
+        conexion.close()
+
         pass
 
 
