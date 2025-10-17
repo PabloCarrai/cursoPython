c7c5acfe3e19b2d81e325cf7174dbb8c7e222f7a
PabloCarrai <pablohernanjuan@hotmail.com>
2025-10-17 17:05:08 -0300

    Continuando esto

---
+++ Recursividad/Ejercicio-308.py
@@ -0,0 +1,9 @@
+def factorial(fact):
+    if fact > 0:
+        valor = fact * factorial(fact - 1)
+        return valor
+    else:
+        return 1
+
+
+print(f"El factoriald e 4 es {factorial(4)}")
